bfa7f323904f66f7eec6cf7d7ff80379894e9f97
Ayesha Nadeem <an0124163@gmail.com>
2026-07-06 13:09:27 +0500

    calculator

---
+++ Day1/calculator.py
@@ -0,0 +1,12 @@
+#x = int(input(" Enter First value: "))
+#y = int(input(" Enter Second value: ")) 
+
+x = float(input(" Enter First value: "))
+y = float(input(" Enter Second value: ")) 
+
+#z= round(x+y)
+#print(f"{z:,}")
+
+#z = round( x / y , 2 )
+z = x / y
+print(f"{z:.2f}")

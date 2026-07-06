c1f9135a9cc2fb14090ee2c3cf8e1ec0d7bf9e0f
Ayesha Nadeem <an0124163@gmail.com>
2026-07-06 12:44:20 +0500

    string functions

--- Day1/hello.py
+++ Day1/hello.py
@@ -1,5 +1,13 @@
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title() # Ask user for their name
 
-print("hello, ", name)
-print(name)
-print("hello, {name}")
+#name = name.strip().title() # Remove whitespaces from str and title
+
+#name = name.title() # Capitilize
+first , last = name.split(" ") #split user name into first name and last name 
+
+#print("hello, ", name , " Welcome! ", end="", sep="   ") # Say hello to user  end="\n" separator=" "
+#print(name)
+
+#print("hello , \"Ayesha\"")
+
+print(f"hello, {first}")

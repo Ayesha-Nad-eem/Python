09a8e445ad4bd5cebad20df8a62d46eb572ef05d
Ayesha Nadeem <an0124163@gmail.com>
2026-07-09 16:02:25 +0500

    Dictionary

---
+++ Day3/dictionary.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermoine" : "Gryffindor",
+    "Harry" : "Gryffindor",
+    "Ron" : "Gryffindor",
+    "Draco" : "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep= " : ")
